fdb497576f11ed370631dddb60604da14e49295f
Natalie-Cardot <nataliecardot@gmail.com>
2018-08-27 15:04:46 -0700

    feat: Add restaurant hours to tab order

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -12,6 +12,7 @@ class DBHelper {
   static fetchRestaurants(callback) {
     let xhr = new XMLHttpRequest();
     xhr.open('GET', DBHelper.DATABASE_URL);
+    // Function called when an XMLHttpRequest transaction completes successfully
     xhr.onload = () => {
       if (xhr.status === 200) { // Got a success response from server!
         const json = JSON.parse(xhr.responseText);
