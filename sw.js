84d45c226b5d157681844029d912c30a62e34066
Natalie-Cardot <nataliecardot@gmail.com>
2018-08-31 23:17:29 -0700

    Edit comments, add missing semicolons

--- sw.js
+++ sw.js
@@ -2,8 +2,8 @@
 
 // Not placing this file in js folder because for security reasons, a service worker can only control the pages that are in its same directory or its subdirectories (must cache assets outside of js folder)
 
-// TODO: Check this is right
-const cacheName = 'restaurant-cache';
+// Must work with service worker to pick up changes to CSS, etc., by changing service worker so browser treats updated service worker as new version, which gets its own install event in which it fetches assets (including updated ones) and places them in a new cache, which isn't automatic (have to change name of cache). Create new cache as not to disrupt one already being used by old service worker and pages it controls. Delete old cache once old service worker is released
+const cacheID = 'mws-restaurant-v1';
 
 const cacheAssets = [
   '/',
@@ -27,14 +27,14 @@ const cacheAssets = [
   '/img/10.jpg'
 ];
 
-// The install event listener opens caches object and populates it with list of resources to cache. cache.addAll() will reject if any resources fail to cache, preventing service worker from installing
+// The install event listener opens caches object and populates it with list of resources to cache. cache.addAll() will reject if any resources fail to cache, preventing service worker from installing (in other words it's atomic). Also addAll() uses fetch; requests transmitted via browser cache
 self.addEventListener('install', e => {
   console.log('Service worker installed!');
 
-  // event.waitUntil() extends lifetime of install event until passed promise resolves successfully (note: a "settled" promise is either fulfilled [resolve() was called] or rejected [reject() was called], but not pending. Sometimes "resolved" and "settled" are used synonymously to mean not pending. A "resolved" promise is one that is settled to match state of another promise, meaning the then function will be called). If promise rejects, service worker is abandoned
+  // event.waitUntil() enables us to signal progress of install. It also extends lifetime of install event until passed promise resolves successfully (note: a "settled" promise is either fulfilled [resolve() was called] or rejected [reject() was called], but not pending. Sometimes "resolved" and "settled" are used synonymously to mean not pending. A "resolved" promise is one that is settled to match state of another promise, meaning the then function will be called). If promise rejects, service worker is abandoned
   e.waitUntil(
-    caches.open('cacheName').then(cache => {
-      console.log('Service worker caching files!')
+    caches.open(cacheID).then(cache => {
+      console.log('Service worker caching files!');
       return cache.addAll(cacheAssets);
     });
   );
@@ -54,9 +54,9 @@ self.addEventListener('activate', e => {
             console.log('Service worker deleting old cache');
             return caches.delete(cache);
           }
-        })
+        });
       );
-    })
+    });
   );
 });
 
@@ -66,10 +66,10 @@ self.addEventListener('activate', e => {
 self.addEventListener('fetch', (e => {
   console.log(e.request.url);
 
-  // "The event.respondWith() method tells the browser to evaluate the result of the event in the future. caches.match(event.request) takes the current web request that triggered the fetch event and looks in the cache for a resource that matches. The match is performed by looking at the URL string. The match method returns a promise that resolves even if the file is not found in the cache. This means that you get a choice about what you do. In your simple case, when the file is not found, you simply want to fetch it from the network and return it to the browser. https://developers.google.com/web/fundamentals/codelabs/offline/"
+  // event.respondWith() method tells browser to evaluate result of event in the future. caches.match(event.request), which is used to remove something from cache, searches cache for resource matching current web request that triggered fetch event, based on URL string, and returns a promise that resolves even if file is not found in cache, unlike cache.match, caches.match tries to find match in any cache, starting with oldest
   e.respondWith(
     caches.match(e.request).then(response => {
       return response || fetch(e.request);
-    })
+    });
   );
 }));
