50abe76a9c8b1b7c7a0140701e3d0da5fa410005
Natalie-Cardot <nataliecardot@gmail.com>
2018-08-31 22:40:06 -0700

    fix: Correct service worker file path

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// Checks if browser supports service workers (as of 8/18 all versions of IE and Opera Mini [Android version of Opera] do not). Navigator.serviceWorker is read-only property that returns ServiceWorkerContainer object for associated document, which provides access to registration, removal, upgrade, and communication with service worker
-if ('serviceWorker' in navigator) {
-  // Registers service worker script. Returns a promise
-  // then() method returns a Promise. It takes up to two arguments: callback functions for the success and failure cases of the Promise
-  navigator.serviceWorker.register('/sw.js').then(() => {
-    console.log('Registration successful');
-  // catch() method returns a Promise and deals with rejected cases only. It's equivalent to then(undefined, func), but used in its place for readability
-  }).catch(err => {
-    console.log(`Service worker registration failed; error: {$err}`);
-  });
-}
-
 let restaurants;
 let neighborhoods;
 let cuisines;
@@ -180,5 +168,17 @@ addMarkersToMap = (restaurants = self.restaurants) => {
       window.location.href = marker.options.url;
     }
     self.markers.push(marker);
+  });
+}
+
+// Checks if browser supports service workers (as of 8/18 all versions of IE and Opera Mini [Android version of Opera] do not). Navigator.serviceWorker is read-only property that returns ServiceWorkerContainer object for associated document, which provides access to registration, removal, upgrade, and communication with service worker
+if ('serviceWorker' in navigator) {
+  // Registers service worker script. Returns a promise
+  // then() method returns a Promise. It takes up to two arguments: callback functions for the success and failure cases of the Promise
+  navigator.serviceWorker.register('../sw.js').then(() => {
+    console.log('Registration successful');
+  // catch() method returns a Promise and deals with rejected cases only. It's equivalent to then(undefined, func), but used in its place for readability
+  }).catch(err => {
+    console.log(`Service worker registration failed; error: {$err}`);
   });
 }
